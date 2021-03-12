383df46f26234c10beff7d220a232208e1384de9
disini <disini@live.cn>
2021-03-12 16:26:09 +0800

    fix error: set up envirionments before invoke meshroom_batch program!

---
+++ setEnv.sh
@@ -0,0 +1,10 @@
+export PATH=$PATH:$PWD/aliceVision/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib:$PWD/aliceVision/lib 
+export PYTHONPATH=$PWD/lib 
+
+# run!:
+#/usr/bin/python3m bin/meshroom_batch --input /home/ls/projects/3d_Recon/datasets/photos/Images-20210128T092219Z-001/Images -o /home/ls/projects/3d_Recon/output/Meshroom/results/202103121037
+
+
+
+#PATH=$PATH:$PWD/aliceVision/bin LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib:$PWD/aliceVision/lib PYTHONPATH=$PWD/lib /usr/bin/python3m bin/meshroom_batch --input /home/ls/projects/3d_Recon/datasets/photos/Images-20210128T092219Z-001/Images -o /home/ls/projects/3d_Recon/output/Meshroom/results/202103121415
